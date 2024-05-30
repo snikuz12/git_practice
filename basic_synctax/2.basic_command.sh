9afe9e0216130045c3e50709dadef2e8ddf44236
snikuz12 <snikuz12@naver.com>
2024-05-30 10:12:54 +0900

    fix

---
+++ basic_synctax/2.basic_command.sh
@@ -0,0 +1,67 @@
+# 현재 working directory, staging area만 상태 확인
+git status
+
+# 모든 수정, 추가사할 add
+git add .
+
+# git add 특정파일 : 특정 파일 add
+git add 특정파일(위치포함)
+
+# local repository에 반영 및 커밋이력 생성
+git commit -m "메시지 타이틀" -m "메시지 내용"
+
+# vi 모드에서 작성 : git commit 후 엔터
+
+# add와 commit 동시에
+git commit -am "커밋메시지"
+
+# commit 이력 확인 (local repsitory의 이력)
+git log
+
+# commit log 간결하게
+git log --oneline
+
+# 전체 이력 조회
+git log --all
+
+# 로그를 그래프 형태로 조회
+git log --graph
+
+# 원격저장소로 업로드
+git push origin 브랜치명
+
+# 충돌 무시하고 강제 push
+git push origin main --force
+
+# 특정 commit ID로의 전환
+git checkout 커밋ID
+
+# 다시 현재의 master(브랜치)의 commit로 return
+git checkout main
+
+# 원격의 변경사항을 local로 다운로드
+git pull origin 브랜치명
+
+# git 작업 취소
+# working directory에서의 취소
+git checkout .
+
+# staging area에서의 취소
+git reset
+
+# commit 이후의 취소
+git reset HEAD~1
+git reset HEAD^
+
+# push 이후의 취소
+git revert 커밋ID
+
+# 버전별 비교 명령어 : diff
+# A를 기준으로 B가 어떤 변경이 있는지를 비교
+git diff A B
+git diff commitId1 commitId2
+git diff A브랜치 B브랜치
+
+# git fetch
+# 원격의 변경사항을 로컬에 가져오되, 병합은 하지 않음
+# 비교 : git pull origin main과 git fetch origin main
--- basic_synctax/env_setting.sh
+++ basic_synctax/env_setting.sh
@@ -1,37 +1,67 @@
-# git add . 은 모든 변경 사항 staging area 이동
+# git add . 은 모든 변경사항 staging area로 이동
 git add .
 
-# git commitd은 staging area의 변경사항을 확정짓고 commitID 생성
-git commit -m "message"
+# staging area의 변경사항을 확정 짓고 commit ID 생성
+git commit -m "메시지"
 
 # 원격 repo에 upload
 git push origin 브랜치명
 
 # github 인증방법 2가지
 # 1. token 인증방식 : github에서 직접 보안키(token) 발급
+# 2. 제3자 인증방식 
 
-# 2. 제3자 인증(oauth)방식
-
-# git 프로젝트 생성방법 2가지
-# 방법1. 원격 repo 생성  후 clone
+# git 프로젝트 생성 방법 2가지
+# 방법1. 원격 repo 생성 후 clone
 
 # 방법2. 로컬에서부터 이미 개발된 프로젝트가 존재시에 github 업로드
-# 해당 프로젝트 경로에 .git폴더 생성
-git init
+# 해당 프로젝트 경로에 .git 폴더 생성
+git init 
 
-# 원격지 주소를 생성 및 추가
+# 원격지 주소 생성 및 추가
 # 원격지 주소 추가
-git remote add origin 레포주소
+# origin = 원격
+git remote add origin 리포지토리 주소
 
 # 원격지 주소 삭제
 git remote remove origin
 
-# 원격주소 변경
-git remote set-url origin 레포주소
+# 원격지 주소 변경
+git remote set-url origin 변경할 리포지토리 주소
 
-# git 설정정보 조회
+# git 설정 정보 조회
 git config --list
 
-# 1. 레포 kimseonguk197 깃 주소에서 아무거나 여러분 repo로 갖고가기
-#     1) commit id를 유지한채 가져가기(팀작업후 가져가기)
-#     2) commit id 를 전부 없애고 내 레포 인것처럼 가져가기
+# 타인레포 clone 방법 2가지
+# 커밋 이력 그대로 가져오기
+git clone 타인레포주소
+git remote set-url origin 레포주소
+git push origin master # 별도의 add, commit 필요 없음
+# 커밋 이력없이 가져오기
+git clone 타인레포주소
+rm .git
+git remote add origin 레포주소
+git add .
+git commit -m "first project"
+
+# 사용자 지정 방법
+# 전역적 사용자(이름, 이메일) 지정
+git config --global user.name "이름"
+git config --global user.email "이메일"
+
+# 지역적 사용자(이름, 이메일) 지정
+# 현재 레파지토리에 한해서만 사용자 지정
+git config --local user.name "이름"
+git config --local user.email "이메일"
+
+# repository, 이름, email ..
+git config --list
+
+# 사용자 이름 조회
+git config user.name "이름"
+
+# 사용자 이메일 조회
+git config user.email "이메일"
+
+# git ignore 파일 실습
+# 확장자 .gitignore 파일 생성 후 git 추적 제외 대상 나열
